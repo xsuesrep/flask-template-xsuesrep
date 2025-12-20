6412d2bb3d524e904bdaa5a55b84842b508ff084
xsuesrep <std.67122420215@ubru.ac.th>
2025-12-20 20:40:18 +0700

    first commit

---
+++ server.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    title = 'Home Page'
+    return render_template('index.html', title=title)
+
+@app.route('/about')
+def about():
+    title = 'About Page'
+    name = 'Nattawut'
+    email = 'std.67122420215@ubru.ac.th'
+    mobile = '0836804693'
+    age = 20
+    return render_template('about.html',
+                            name=name,
+                            email=email,
+                            mobile=mobile,
+                            age=age)
+
+@app.route('/favorite/foods')
+def favorite_foods():
+    title = 'Favorite Foods'
+    foods = ['Pad kraprao', 'moo ping', 'Kai tod', 'Tom Kha Gai', 'Pad Thai']
+    return render_template('favorite_foods.html',
+                            foods=foods)
+
+@app.route('/favorite/spots')
+def favorite_spots():
+    title = 'Favorite Spots'
+    spots = ['Basketball', 'Boxing', 'Football']
+    return render_template('favorite_spots.html',
+                            spots=spots)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
